f3791ec10df40b8eb6f2a4b4dff24ea8095f1570
ledon <powertle@outlook.com>
2024-08-08 21:01:22 +0800

    重构 H264Packet 结构体并添加新引用
    在 `H264Packet.cs` 文件中：
    - 移除了 `using Pitaya.Buffers;` 语句。
    - 添加了 `using Pitaya.BasicType.BitTypes;`、`using Pitaya.Buffers;` 和 `using System.Text.Json;` 语句。
    - 修改了 `H264Packet` 结构体的定义，使其实现了 `IPitayaPlayloadPacket`、`IPitayaPacket<H264Packet>` 和 `IPitayaAnalyzable` 接口。
    - 为 `H264Packet` 结构体添加了 `Analyze`、`Decode` 和 `Encode` 方法，这些方法目前抛出 `NotImplementedException` 异常。
    - 在 `H264Packet` 结构体中添加了一个嵌套的 `NALUnitPacket` 结构体，该结构体实现了 `IPitayaPacket<NALUnitPacket>` 和 `IPitayaAnalyzable` 接口。
    - 为 `NALUnitPacket` 结构体添加了 `ForbiddenZoreBit`、`NalRefIdc` 和 `NalUnitType` 属性，以及 `Analyze`、`Decode` 和 `Encode` 方法，这些方法目前也抛出 `NotImplementedException` 异常。
    
    在 `Pitaya.VideoCodec.csproj` 文件中：
    - 添加了对 `..\Pitaya.BasicType\Pitaya.BasicType.csproj` 项目的引用。

--- src/Pitaya.VideoCodec/Frames/H264Packet.cs
+++ src/Pitaya.VideoCodec/Frames/H264Packet.cs
@@ -1,12 +1,60 @@
-﻿using Pitaya.Buffers;
+﻿using Pitaya.BasicType.BitTypes;
+using Pitaya.Buffers;
+using System.Text.Json;
 
 namespace Pitaya.VideoCodec.Frames;
 
-public struct H264Packet : IPitayaPlayloadPacket
+public struct H264Packet : IPitayaPlayloadPacket, IPitayaPacket<H264Packet>, IPitayaAnalyzable
 {
     public H264Packet()
     {
     }
 
     public byte[] Origin { get; set; } = [];
+
+    public void Analyze(ref BufferReader reader, Utf8JsonWriter writer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public H264Packet Decode(ref BufferReader reader)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Encode(ref BufferWriter writer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public struct NALUnitPacket : IPitayaPacket<NALUnitPacket>, IPitayaAnalyzable
+    {
+        public NALUnitPacket()
+        {
+        }
+
+        public Bit ForbiddenZoreBit { get; set; } = Bit.Zero;
+        public Bit2 NalRefIdc { get; set; } = Bit2.Zero;
+        /// <summary>
+        /// bit 5
+        /// </summary>
+        public byte NalUnitType { get; set; }
+
+        public byte[] Origin { get; set; } = [];
+
+        public void Analyze(ref BufferReader reader, Utf8JsonWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NALUnitPacket Decode(ref BufferReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Encode(ref BufferWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }

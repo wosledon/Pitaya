52ccfc97ffd099b73f3c8f4145622668a7e8a9b4
ledon <powertle@outlook.com>
2024-08-26 17:07:46 +0800

    新增 RtspClient 枚举、构造函数及异步方法
    在 `RtspClient.cs` 文件中进行了以下更改：
    - 添加了 `System.Net` 的 `using` 语句。
    - 新增 `RtspConnectType` 枚举，包含 `TCP`、`UDP` 和 `Http` 三种连接类型。
    - 在 `RtspClient` 类中新增了两个构造函数：
      - `public RtspClient(string url, RtspConnectType connectType = RtspConnectType.TCP)`
      - `public RtspClient(IPEndPoint iPEndPoint, string username, string passwaord)`
    - 新增了以下异步方法：
      - `public Task ConnectAsync()`
      - `public Task PlayAsync()`
      - `public Task PauseAsync()`
      - `public Task StopAsync()`
      - `public Task CloseAsync()`
    - 新增了以下事件处理方法：
      - `public void OnMessageReceived(Action<string> action)`
      - `public void OnError(Action<Exception> action)`
    - 删除了 `RtspServer` 类中的一个空行。

--- src/Pitaya.Rtsp/RtspClient.cs
+++ src/Pitaya.Rtsp/RtspClient.cs
@@ -1,11 +1,58 @@
-﻿namespace Pitaya.Rtsp;
+﻿using System.Net;
+
+namespace Pitaya.Rtsp;
+
+public enum RtspConnectType
+{
+    TCP,
+    UDP,
+    Http
+}
 
 public class RtspClient
 {
+    public RtspClient(string url, RtspConnectType connectType = RtspConnectType.TCP)
+    { }
 
+    public RtspClient(IPEndPoint iPEndPoint, string username, string passwaord)
+    { }
+
+    public Task ConnectAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PlayAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PauseAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CloseAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnMessageReceived(Action<string> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnError(Action<Exception> action)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public class RtspServer
 {
-
 }
